a59af905e9a5ad7186882cdc530421984bbd5373
周睿 <zrufo747@outlook.com>
2026-03-10 17:26:27 +0800

    ✨ feat: 更新测试名称为 test_e1000，并添加相应的测试实现

--- scripts/test_e1000.sh
+++ scripts/test_e1000.sh
@@ -9,7 +9,7 @@ QEMU_CONFIG="$PROJECT_ROOT/driver/eth/intel/qemu.toml"
 cd "$PROJECT_ROOT" || exit 1
 
 cargo t --manifest-path ./driver/eth/intel/Cargo.toml \
-	--test test \
+	--test test_e1000 \
 	--target aarch64-unknown-none-softfloat \
 	-- \
 	--show-output \
